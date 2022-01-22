4b5e1d274a64e81578fc71431f6c96a97c724c70
Sridhar Rajaram <srilumia@hotmail.com>
2022-01-22 15:09:30 +0530

    Mongoose to Mongodb Connection

--- server.js
+++ server.js
@@ -6,28 +6,72 @@ const app = express();
 
 const cors = require('cors');
 
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const PORT = process.env.PORT || 5000;
-const URI = process.env.MONGO_URL;
-
-
-
-const expenseRouter = require('./router/expense');
+const DBURL = process.env.MONGO_URL;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect(URI, {
-    useNewUrlParser: true
-});
+app.get("/expense", async function (req, res) {
 
-const connection = mongoose.connection;
-connection.once('open', ()=> {
-    console.log('db connected successfully');
-});
+    // mongodb Database concept introduced
+    try {
+        //connect the database
+        let client = await mongoClient.connect(DBURL) //since it is returning the promise, we are puting in try catch async
 
-app.use('/expense',expenseRouter);
+        //select the db
+        let db = client.db("moneymanager")
+
+        //select the collection and perform the action
+        let data = await db.collection("expenses").find().toArray() //since it is returning the promise we put await, what see is cursor pointer, so toArray
+
+        //close the database
+        await client.close();
+
+        res.json(data) //reply with data
+
+    } catch (error) {
+        res.status(500).json({
+            message: "something went wrong"
+        })
+    }
+
+})
+
+app.post("/expense/addExpense", async function (req, res) {
+
+    // mongodb Database concept introduced
+    try {
+        //connect the database
+        let client = await mongoClient.connect(DBURL) //since it is returning the promise, we are puting in try catch async
+
+        //select the db
+        let db = client.db("moneymanager")
+
+        //select the collection and perform the action
+        //req.body.userid = req.userid;
+        let data = await db.collection("expenses").insertOne(req.body) //since it is returning the promise we put await
+
+        //close the database
+        await client.close();
+
+        res.json({
+            message: "Expense added sucessfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "something went wrong"
+        })
+    }
+
+})
 
 app.listen(PORT,()=>{
     console.log(`app is running on port ${PORT}`);
